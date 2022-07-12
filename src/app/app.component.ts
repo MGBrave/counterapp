ee0293a2b725057b10e7ee60a4a4f07faf771a96
Marta Geraldo <60014891+martageraldo@users.noreply.github.com>
2022-07-12 18:44:35 -0300

    contador simples em angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'counterapp';
+
+  countNumber = 0;
+
+  resetBtn(){
+    this.countNumber = 0;
+  }
+
+  //increase
+  increaseBtn(){
+    this.countNumber = this.countNumber + 1;
+    
+  }
+
+  //decrease
+  decreaseBtn(){
+    this.countNumber = this.countNumber - 1;
+    if(this.countNumber > 0){
+      this.countNumber = 0;
+    }
+  }
 }
